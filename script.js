672f3393f468615673554518a6424cc256d44435
GameBenderStudios <146949186+GameBenderStudios@users.noreply.github.com>
2024-01-03 15:53:30 +0300

    additions
    new help menu
    revamped clear command
    doom
    new create command

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentDirectory = 'C:\\';
     const commandHistory = [];
     let historyIndex = 0;
-    let directories = [ '']; // Array to store directories
+    const files = [];
+    let directories = []; // Array to store directories
+
 
     input.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
@@ -13,20 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
             historyIndex = 0;
             input.value = '';
             if (command === 'help') {
-                output.innerHTML += `<br>${currentDirectory}> Available commands: <br>1. help - Show available commands <br>2. date - Display current date <br>3. time - Display current time <br>4. clear - Clear the screen <br>5. echo [message] - Display a message <br>6. ls - List files and directories <br>7. cd [directory] - Change directory <br>8. cat [filename] - View the contents of a text file <br>9. download [filename] - Download a text file <br>10. mkdir [directory] - Create a new directory`;
+                output.innerHTML += `<br>${currentDirectory}> Available commands: <br>HELP                       Show available commands <br>DATE                       Display current date <br>TIME                       Display current time <br>CLS                        Clear the screen <br>ECHO [message]             Display a message <br>LS                         List files and directories <br>CD [directory]             Change directory <br>CAT [filename]             View the contents of a text file <br>DOWNLOAD [filename]        Download a text file <br>MKDIR [directory]          Make a new directory. <br>CREATE [file name]         Creates a new file with .txt, .html, .css or .js extentions.`;
             } else if (command === 'date') {
                 const currentDate = new Date().toLocaleDateString();
                 output.innerHTML += `<br>${currentDirectory}> ${currentDate}`;
             } else if (command === 'time') {
                 const currentTime = new Date().toLocaleTimeString();
                 output.innerHTML += `<br>${currentDirectory}> ${currentTime}`;
-            } else if (command === 'clear') {
+            } else if (command === 'cls') {
                 output.innerHTML = '';
             } else if (command.startsWith('echo ')) {
                 const message = command.slice(5);
                 output.innerHTML += `<br>${currentDirectory}> ${message}`;
             } else if (command === 'ls') {
-                output.innerHTML += `<br>${currentDirectory}> Files: File1.txt File2.txt`;
+                let filesOutput = files.map(file => `${file.name}.${file.extension}`);
+                output.innerHTML += `<br>${currentDirectory}> Files: ${filesOutput.join(' ')} Readme.md`;
                 output.innerHTML += `<br>${currentDirectory}> Directories: ${directories.join(' ')}`;
             } else if (command.startsWith('cd ')) {
                 const newDirectory = command.slice(3);
@@ -35,6 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
             } else if (command.startsWith('cat ')) {
                 const filename = command.slice(4);
                 output.innerHTML += `<br>${currentDirectory}> Contents of ${filename}: This is the content of the file.`;
+            } else if (command === 'doom') {
+                output.innerHTML = '<iframe src="http://browser-doom.io/?page=game&game=doom&profile=2002%3A%20A%20Doom%20Odyssey%20(Freedoom)" width="100%" height="800px"></iframe>'; 
+             
             } else if (command.startsWith('download ')) {
                 const filename = command.slice(9);
                 const blob = new Blob(['This is the content of the file.'], { type: 'text/plain' });
@@ -51,6 +57,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 const directoryName = command.slice(6);
                 directories.push(directoryName); // Add the new directory to the array
                 output.innerHTML += `<br>${currentDirectory}> Creating directory ${directoryName}`;
+            
+            } else if (command.startsWith('create ')) {
+                const fileNameWithExtension = command.slice(7); // Assuming the command is 'create filename.extension'
+                
+                // Extracting the file name and extension
+                const lastDotIndex = fileNameWithExtension.lastIndexOf('.');
+                const fileName = fileNameWithExtension.slice(0, lastDotIndex);
+                const fileExtension = fileNameWithExtension.slice(lastDotIndex + 1);
+
+                // Check if the extension is valid (you can add more extensions as needed)
+                if (['txt', 'js', 'html', 'css'].includes(fileExtension)) {
+                    // Your logic to create the file goes here
+                    const newFile = {
+                        name: fileName,
+                        extension: fileExtension
+                    };
+
+                    // Push the created file into the 'files' array
+                    files.push(newFile);
+
+                    output.innerHTML += `<br>${currentDirectory}> Creating file ${fileNameWithExtension}`;
+                } else {
+                    output.innerHTML += `<br>${currentDirectory}> Invalid file extension: ${fileExtension}`;
+                }
+    
             } else {
                 output.innerHTML += `<br>${currentDirectory}> '${command}' is not recognized as an internal or external command, operable program or batch file.`;
             }
